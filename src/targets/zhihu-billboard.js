2c74f06ee745ac024649dd52ea09b8bfe84bebe3
Jiajun Yan <yanjiajun@ifanr.com>
2021-11-16 13:35:07 +0800

    Update zhihu-billboard.js

--- src/targets/zhihu-billboard.js
+++ src/targets/zhihu-billboard.js
@@ -2,19 +2,17 @@
  * 知乎热榜
  */
 
-const cheerio = require('cheerio')
 const axios = require('axios')
 
 const { loadLocal, getTodaysDate, readJSON, exists } = require('../common')
 const { CATEGORY } = require('../constants')
-const source = loadLocal('./sample/zhihu-billboard.html')
+// const source = loadLocal('./sample/zhihu-billboard.html')
 
 const crawler = async () => {
   const url =
     'https://www.zhihu.com/api/v3/feed/topstory/hot-lists/total?limit=100'
   const { data: source } = await axios.get(url)
 
-  console.log(source.data)
   const target = source.data.map(item => ({
     title: item.target.title,
     description: item.target.excerpt,
