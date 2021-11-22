6042ee4c648d7ff4d0120d01fdab4134f09ff7d8
Jiajun Yan <yanjiajun@ifanr.com>
2021-11-22 18:08:36 +0800

    Update git.js

--- src/git.js
+++ src/git.js
@@ -6,38 +6,31 @@ const { CATEGORY_MAP } = require('./constants')
 
 const git = simpleGit()
 
-const save = ({ category, result, suffix }) => {
+const setJSONPath = (category, suffix) => {
   const today = getTodaysDate()
-  writeJSON(
-    `./results/${category}/${today}${suffix ? '-' + suffix : ''}.json`,
-    result
-  )
-  writeJSON(
-    `./results/${category}/latest${suffix ? '-' + suffix : ''}.json`,
-    result
-  )
+  suffix = suffix ? '-' + suffix : ''
+  const withDate = `./results/${category}/${today}${suffix}.json`
+  const latest = `./results/${category}/latest${suffix}.json`
+
+  return { withDate, latest }
+}
+
+const save = ({ category, result, suffix }) => {
+  const { withDate, latest } = setJSONPath(category, suffix)
+  writeJSON(withDate, result)
+  writeJSON(latest, result)
 }
 
 const push = async ({ category, result, suffix = '' } = {}) => {
   save({ category, result, suffix })
-  const today = getTodaysDate()
+  const { withDate, latest } = setJSONPath(category, suffix)
 
   try {
     await git.pull()
     await git
       .init()
-      .add(
-        path.join(
-          __dirname,
-          `./results/${category}/${today}${suffix ? '-' + suffix : ''}.json`
-        )
-      )
-      .add(
-        path.join(
-          __dirname,
-          `./results/${category}/latest${suffix ? '-' + suffix : ''}.json`
-        )
-      )
+      .add(path.join(__dirname, withDate))
+      .add(path.join(__dirname, latest))
       .commit(`feat: update ${CATEGORY_MAP[category]}`)
       .push()
   } catch (error) {
